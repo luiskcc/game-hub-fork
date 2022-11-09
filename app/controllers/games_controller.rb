7d26265218a0bbf142be69ec5f3b93b6e7f51404
luiskcc <114909465+luiskcc@users.noreply.github.com>
2022-11-09 07:19:11 +0000

    testing json (#3)

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,31 @@ class GamesController < ApplicationController
 
   
   def index
+    data = {
+"games": [
+{
+"id": 1,
+"title": 'COD',
+"link": Rails.application.routes.url_helpers.game_path(1)
+},
+{
+"id": 2,
+"title": 'The Last Of Us',
+"link": Rails.application.routes.url_helpers.game_path(2)
+}
+]
+}
+
+render json: data
   end
 
+
+
   def show
-  end
+    movie_id = params[:id]
+
+    render json: "Example of a movie with id #{movie_id}"
+  
+end
+
 end
